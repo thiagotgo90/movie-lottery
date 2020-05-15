9d4793515a8ecaff475270a073aff36833ed9256
Thiago Oliveira <thiago.tgo90@gmail.com>
2020-05-15 07:06:18 +0100

    CKD workshop

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,19 +1,33 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2'
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const instance = new ec2.Instance(this, 'NewsBlogInstance', {
-      vpc: new ec2.Vpc(this, 'VPC'),
-      machineImage: new ec2.AmazonLinuxImage(),
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO)
+    
+    const hello = new lambda.Function(this, 'Hello', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler"
+    });
+    
+    const helloWithCounter = new HitCounter(this, "helloWithCounter", {
+      downstream: hello
+    })
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, "TableViewer", {
+      table: helloWithCounter.table,
+      title: 'Hello Hits',
+      sortBy: '-hits'
     })
   }
 }
